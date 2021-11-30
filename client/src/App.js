88af3309aafcba7ae6c2ccd084efc3f6e5604ddc
cjs0533 <choec53@gmail.com>
2021-11-30 17:42:00 +0900

    20211130

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './assets/css/main.css';
 import './assets/css/sub.css';
 import Header from './main/Header';
-// import Banner from './main/Banner';
+import Banner from './main/Banner';
 
 // import SectionOne from './main/SectionOne';
 // import SectionTwo from './main/SectionTwo';
@@ -11,17 +11,19 @@ import Header from './main/Header';
 
 import Footer from './main/Footer';
 import ApiMain from './api/ApiMain';
-
+import { Route } from 'react-router-dom';
 
 
 
 function App() {
   return (
     <>
-
-      <Header color='white' cursor='none' />
+    <Header color='white' cursor='none' />
+      {/* <Route path="" component={Banner} exact/> */}
+      {/* <Route path="/apart" component={ApiMain} /> */}
+      
+      
       <ApiMain />
-      {/* <DatePickerComponent/> */}
       {/* <Banner /> 
       
       <SectionOne />
