c96c1e6e438a7bfeee298187181be755000f8239
cjs0533 <choec53@gmail.com>
2021-11-30 17:08:26 +0900

    20211130

---
+++ client/src/include/Loading.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../loading.css';
+
+function Loading() {
+  return (
+    <>
+        <div className="loading_bx" >
+            <div className="loading">
+                <div className="loadingio-spinner-dual-ring-9cvfyzm989d">
+                    <div className="ldio-ow7acjar329">
+                    
+                    </div>
+                </div>
+                    
+                <span>Loading. . .</span>
+            </div>
+        </div>
+    </>
+  );
+  
+}
+
+export default Loading;
